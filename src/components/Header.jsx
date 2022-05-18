64d6430f5f0825991eebcca8f328c237c32af9ba
ksfgn <shapovalovalex938@gmail.com>
2022-05-18 21:20:17 +0300

    layout made

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => (
+  <header className="header">
+    <img
+      src="https://www.logo.wine/a/logo/WeChat/WeChat-Icon-Light-Grey-Logo.wine.svg"
+      alt=""
+    />
+  </header>
+);
+export default Header;
